51d609df87c209170649f6ea00646f985a74139e
Bruno Tarmann <bruno@tarmann.com.br>
2015-09-18 11:07:38 +0100

    render characters by type

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var byKey = function(obj, key){
+var objKey = function(obj, key){
   return key;
 };
 
@@ -11,16 +11,63 @@ var attr = function(value){
 var getAttributes = function(attribute){
   return _(MDM_CARDS)
     .groupBy(attr(attribute))
-    .map(byKey)
+    .map(objKey)
     .value();
 };
 
-var characters   = getAttributes("cardName");
+var fileName = function(str){
+  return str
+    .toLowerCase()
+    .replace("-","_")
+    .replace(".","")
+    .replace(" ","_");
+};
+
+var byCharacters = function(item){
+  return (item.subtitle !== "Basic Action Card");
+};
+
+var costs = function(cards){
+  return _(cards)
+    .sortBy(attr('cost'))
+    .map(attr('cost'))
+    .uniq()
+    .value()
+    .join(', ');
+};
+
+var character = function(cards, key){
+  return {
+    name: key,
+    type: cards[0].type,
+    affiliation: cards[0].affiliation,
+    cost: costs(cards),
+    thumbnail: fileName(key)
+  };
+};
+
+var getCharacters = function(cards){
+  return _(cards)
+    .filter(byCharacters)
+    .sortBy(attr('type'))
+    .groupBy(attr("cardName"))
+    .map(character)
+    .value();
+};
+
+var characters   = getCharacters(MDM_CARDS);
 var affiliations = getAttributes("affiliation");
 var types        = getAttributes("type");
 var rarities     = getAttributes("rarity");
 
-console.log(characters);
-console.log(affiliations);
-console.log(types);
-console.log(rarities);
+var render = function(){
+  $.each(characters, function(i, character){
+    $("#characters").append([
+      '<div class="character col-xs-2 type-',fileName(character.type),'">',
+      '<img class="img-responsive" src="images/characters/', character.thumbnail ,'.jpg" alt="',character.name,'"/>',
+      '</div>'
+    ].join(''))
+  });
+};
+
+render();
